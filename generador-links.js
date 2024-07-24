80a2a51669102e3638a5f41b25b0a29d0f2d8bdc
Leandro Avila - L3anAv <leandroav.dev@gmail.com>
2024-07-24 19:40:53 -0300

    fix: origin=*

--- generador-links.js
+++ generador-links.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: 'https://generador-links.vercel.app/',
+  origin: '*',
   methods: ['GET', 'POST'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
   exposedHeaders: ['Content-Range'],
